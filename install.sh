b947ec204b943513568d49e60f7599ce2197e61a
Daniel Jendinger <hexabit@MacBook-Pro.local>
2024-05-23 12:58:07 +0200

    ...

--- install.sh
+++ install.sh
@@ -5,24 +5,24 @@ latest_version=$(curl -fsSL "https://desktop.figma.com/mac/version.txt")
 intel_url="https://desktop.figma.com/mac/Figma-${latest_version}.zip"
 apple_silicon_url="https://desktop.figma.com/mac-arm/Figma-${latest_version}.zip"
 echo "######################################"
-echo "#         Figma-Installer            #"
-echo "#      Daniel Jendinger 2023         #"
+echo "#         KICKS Mac-Installer            #"
+echo "#      Daniel Jendinger 2024         #"
 echo "######################################"
 echo ""
 echo "Har du en (1) Intel eller (2) Apple Silicon-Mac?"
 read -p "1 eller 2:" comp_type
 
 if [ "$comp_type" == "1" ]; then
-    echo "Laddar ner Figma Intel-verionen"
+    echo "Installerar för Intel-verionen"
     curl $intel_url --output test.zip
 
 else
 	curl $apple_intel_url --output test.zip
-	echo "Laddar ner Figma Silicon-verionen"
+	echo "Installerar Silicon-verionen"
 fi
-echo "Installerar Figma under Applications.."
+echo "Laddar ner och installerar.."
 unzip test.zip
-cp -r figma.app /Applications/
-rm -r figma.app
-rm test.zip
+#cp -r figma.app /Applications/
+#rm -r figma.app
+#rm test.zip
 echo "Klart!!"
